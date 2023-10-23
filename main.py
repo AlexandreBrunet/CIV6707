f74aa6c923121950db89c6ebf0fd87bf8e6e4fd8
Alexandre Brunet <alexandrebrunet@Alexandres-MBP.hitronhub.home>
2023-10-23 17:45:09 -0400

    refactoring

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import overpy
-from QueryToGeojson import convert_ways_to_geojson
-from QueryToGeojson import convert_nodes_to_geojson
-from QueryToGeojson import write_geojson
-from OverpassQuery import *
+from querytogeojson import convert_ways_to_geojson
+from querytogeojson import convert_nodes_to_geojson
+from querytogeojson import write_geojson
+from overpassquery import *
 import datetime
+import re
 
 
 api = overpy.Overpass()
 
 current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-queries = [query1, query2]
+queries = [query1]
 
 
 for index, query in enumerate(queries):
 
-    output_filename = f"query_{index}_{current_date}"
-
     if query.strip().endswith("out geom;"):
+
+        pattern = r'way\[[^\]]+\]'
+        output_filename = re.findall(pattern, query)[0]
 
         geojson = convert_ways_to_geojson(query, api)
         write_geojson(geojson, output_filename)
 
     elif query.strip().endswith("out;"):
+
+        pattern = r'node\[[^\]]+\]'
+        output_filename= re.findall(pattern, query)[0]
 
         geojson = convert_nodes_to_geojson(query, api)
         write_geojson(geojson, output_filename)
